e915ec50a8c121c5acec5c5d30c5c6ca6a829482
Trey Gardner <treygardner72@gmail.com>
2025-04-09 18:40:41 -0600

    Consolidating my R Projects

---
+++ GardnerTreyMidterm.R
@@ -0,0 +1,21 @@
+#CS2020: Intro to statistics for data analysis midterm
+#Trey Gardner
+
+#importing and setup operations
+library("tidyverse")
+library(readr)
+library(dplyr)
+#installing flights data
+install.packages("nycflights13")
+library("nycflights13")
+
+#opening data
+
+#A.) Finding all delayed arrivals and departures (18+, 12+)
+
+
+
+#B.) Finding all summer flights (07-09) Op. JBA from: JFK To: MIA or BQN
+
+
+#C.) Finding subset data, excluding (distance, hour, minute, time_hour)
